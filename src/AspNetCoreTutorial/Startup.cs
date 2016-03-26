a1e59a1b2d0de2e6b02c4a316ae2d5adb8f187db
mikemajesty <mikemajesty2011@gmail.com>
2016-03-26 16:32:01 -0300

    View - 01:41:40

--- src/AspNetCoreTutorial/Startup.cs
+++ src/AspNetCoreTutorial/Startup.cs
@@ -83,7 +83,7 @@ namespace AspNetCoreTutorial
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Foo}/{id?}");
 
                 // Uncomment the following line to add a route for porting Web API 2 controllers.
                 // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
