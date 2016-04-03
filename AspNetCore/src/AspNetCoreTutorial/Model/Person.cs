a2689f04a0033367d5f684bddba66bd6991e8ca2
mikemajesty <mikemajetsy2011@hotmail.com>
2016-04-03 14:37:43 -0300

    Add Person

---
+++ AspNetCore/src/AspNetCoreTutorial/Model/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreTutorial.Model
+{
+    public class Person
+    {
+        [Key]
+        public int? ID { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
